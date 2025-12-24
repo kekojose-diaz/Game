96a638934bc21f03c4422ecc66338ac7aa6e7583
kekojose-diaz <jose.diaz.cantador@gmail.com>
2025-12-24 12:02:12 +0100

    Fox detecting holes

--- Scripts/fox.gd
+++ Scripts/fox.gd
@@ -6,11 +6,18 @@ var direction = 1
 
 @onready var ray_cast_left: RayCast2D = $RayCast_left
 @onready var ray_cast_right: RayCast2D = $RayCast_right
+@onready var ray_cast_down_right: RayCast2D = $RayCast_down_right
+@onready var ray_cast_down_left: RayCast2D = $RayCast_down_left
 @onready var animated_sprite = $AnimatedSprite2D
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if not ray_cast_down_right.is_colliding(): 
+		direction = -1
+		animated_sprite.flip_h = true
+	if not ray_cast_down_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
 	if ray_cast_right.is_colliding():
 		direction = -1
 		animated_sprite.flip_h = true
